ccfc11af2a758fcb17ce05d9737a85258869f29a
Vitor Mello <vitor@vmello.com>
2023-12-23 01:17:07 +0100

    general shell and fish tweaks

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ These are mine. Built for Mac OS X. Certified lit 🔥 🔥 🔥
 
 ## Install on a new machine
 
+**Note that the `init.sh` script is idempotent: it can safely be run multiple times.**
+
 ```bash
 cd ~ && git clone https://github.com/vitormv/dotfiles && cd dotfiles && ./init.sh
 ```
+
 
 > **Important!** Do not forget to reboot the system on the first time you do this
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,4 +5,13 @@ source "$HOME/dotfiles/shell/bash_aliases"
 # override bash reload alias for fish
 alias reload "source ~/.config/fish/config.fish"
 
+# disable welcome greeting when shell starts
 set -U fish_greeting
+
+function fish_right_prompt -d "Show Private label on right side when applicable"
+    if not test -z $fish_private_mode
+        set_color -b purple
+        echo ' PRIVATE '
+        set_color normal
+    end
+end
